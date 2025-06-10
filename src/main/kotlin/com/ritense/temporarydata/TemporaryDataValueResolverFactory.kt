3504c46ed4016aa183d6f1bc8c7a86e637193dcd
haiko van der schaaf <hvdschaaf@gmail.com>
2025-06-10 11:22:23 +0200

    start with valueresolverfactory

---
+++ src/main/kotlin/com/ritense/temporarydata/TemporaryDataValueResolverFactory.kt
@@ -0,0 +1,27 @@
+package com.ritense.temporarydata
+
+import com.ritense.valueresolver.ValueResolverFactory
+import org.camunda.bpm.engine.delegate.VariableScope
+import java.util.function.Function
+
+open class TemporaryDataValueResolverFactory (): ValueResolverFactory {
+    override fun createResolver(documentId: String): Function<String, Any?> {
+        TODO("Not yet implemented")
+    }
+
+    override fun createResolver(processInstanceId: String, variableScope: VariableScope): Function<String, Any?> {
+        TODO("Not yet implemented")
+    }
+
+    override fun handleValues(processInstanceId: String, variableScope: VariableScope?, values: Map<String, Any?>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun supportedPrefix(): String {
+       return PREFIX
+    }
+
+    companion object {
+        const val PREFIX = "td"
+    }
+}
