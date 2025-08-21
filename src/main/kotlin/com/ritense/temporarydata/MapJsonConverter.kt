a775c0aa6df8e6b3e7be458218a148120187f37c
haiko van der schaaf <hvdschaaf@gmail.com>
2025-08-21 10:22:02 +0200

    fix bug for nested properties

--- src/main/kotlin/com/ritense/temporarydata/MapJsonConverter.kt
+++ src/main/kotlin/com/ritense/temporarydata/MapJsonConverter.kt
@@ -3,20 +3,42 @@ package com.ritense.temporarydata
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.persistence.AttributeConverter
-import org.springframework.util.StringUtils
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 
 class MapJsonConverter(
     private val mapper: ObjectMapper
-) : AttributeConverter<Map<String, Any>, String> {
-    override fun convertToDatabaseColumn(map: Map<String, Any>): String {
-        return mapper.writeValueAsString(map);
+) : AttributeConverter<Map<String, Any?>, String> {
+
+    init {
+        // Configure ObjectMapper for better handling of nested objects
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        mapper.configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
     }
 
-    override fun convertToEntityAttribute(json: String): MutableMap<String, Any> {
-        if (!StringUtils.hasText(json)) {
-            return mutableMapOf()
+    override fun convertToDatabaseColumn(map: Map<String, Any?>?): String? {
+        return if (map.isNullOrEmpty()) {
+            null
+        } else {
+            try {
+                mapper.writeValueAsString(map)
+            } catch (e: Exception) {
+                throw IllegalArgumentException("Error converting map to JSON", e)
+            }
         }
-        val typeRef = object : TypeReference<MutableMap<String, Any>>() {}
-        return mapper.readValue(json, typeRef)
+    }
+
+    override fun convertToEntityAttribute(json: String?): MutableMap<String, Any?> {
+        return if (json.isNullOrBlank()) {
+            mutableMapOf()
+        } else {
+            try {
+                val typeRef = object : TypeReference<MutableMap<String, Any?>>() {}
+                mapper.readValue(json, typeRef)
+            } catch (e: Exception) {
+                throw IllegalArgumentException("Error converting JSON to map", e)
+            }
+        }
     }
 }
